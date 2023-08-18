9d3372cb0177092da9c5bce735af8f232952cc29
Sharanov79 <sharanov79@mail.ru>
2023-08-18 12:18:12 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+int usernumber = new int();
+Console.WriteLine("Введите число ");
+usernumber = Convert.ToInt32(Console.ReadLine());
+
+int result = (usernumber * usernumber);
+
+Console.WriteLine($"Степень числа {usernumber} = {result}");
