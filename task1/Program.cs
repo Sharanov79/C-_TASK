3c0b821bd8aa3bf490404b03c5c40dbb594984a6
Sharanov79 <sharanov79@mail.ru>
2023-08-18 12:56:42 +0300

    task2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿/* 
 Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
 Например:
 4 -> 16
@@ -12,3 +12,25 @@ usernumber = Convert.ToInt32(Console.ReadLine());
 int result = (usernumber * usernumber);
 
 Console.WriteLine($"Степень числа {usernumber} = {result}");
+
+/* 
+Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Например:
+a = 5, b = 25 -> да 
+a = 2, b = 10 -> нет 
+a = 9, b = -3 -> нет 
+a = -3 b = 9 -> да
+*/
+
+    Console.WriteLine("Введите число 1: ");
+    int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число 2: ");
+    int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+    if(userNumber2 == userNumber1*userNumber1){
+        Console.WriteLine("true");
+    }else{
+        Console.WriteLine("false");
+    }
+
