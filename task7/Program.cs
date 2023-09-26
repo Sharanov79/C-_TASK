0f7261e2992eb36fa17d9b4c5f5d4bdd73808596
Sharanov79 <sharanov79@mail.ru>
2023-09-26 17:18:41 +0300

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,152 @@
+﻿/*
+Задача 51.
+Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти элементы на их
+квадраты.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Ведите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = GetArray(rows, columns, 0,20);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(ChangeArray(array));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m,n];
+    for (int i=0; i<m; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(min,max);
+        }
+    }
+    return result;
+}
+ int[,] ChangeArray(int[,] array)
+ {
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            if(i%2==0 && j%2==0)
+            {
+                array[i,j] *= array[i,j];
+            }
+        }
+    }
+    return array;
+ }
+ void PrintArray(int[,] array)
+ {
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+ }
+
+/*
+Задача 48.
+Задайте двумерный массив размера m на n,
+каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Ведите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0,20);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m,n];
+    for (int i=0; i<m; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            result[i,j] = i+j;
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+/*
+Задача 46.
+Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Ведите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+PrintArray(GetArray(rows,columns,-9,9));
+
+int[,] GetArray(int rows, int columns, int min, int max)
+{
+    int [,] resultArray = new int[rows,columns];
+    for  (int i=0; i<rows; i++)
+    {
+        for (int j = 0; j<columns; j++)
+        {
+            resultArray[i,j] = new Random().Next(min,max);
+        }
+    }
+    return resultArray;
+}
+void PrintArray(int[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+
+
